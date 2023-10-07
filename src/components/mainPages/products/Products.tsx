0a333b1f5b24281086dfdc885fc46d458832cf1a
Baklay Petro <pustota1988@gmail.com>
2023-10-07 17:18:14 +0300

    fixCSS

--- src/components/mainPages/products/Products.tsx
+++ src/components/mainPages/products/Products.tsx
@@ -34,12 +34,14 @@ interface RootState {
 
 const ProductItem: React.FC<{ product: Product }> = ({ product }) => (
     <div className="product">
-        <GoDotFill/>
+        <GoDotFill />
         <img
-                                src={'https://images.philips.com/is/image/PhilipsConsumer/242V8A_01-IMS-ru_UA?$jpglarge$&wid=960'}
-                                alt={product.title}
-                                className="product__image"
-                            />
+            src={
+                'https://images.philips.com/is/image/PhilipsConsumer/242V8A_01-IMS-ru_UA?$jpglarge$&wid=960'
+            }
+            alt={product.title}
+            className="product__image"
+        />
         <h3 className="product__title">{product.title}</h3>
         <p className="product__serial-number">Serial Number: {product.serialNumber}</p>
         <p className="product__type">Type: {product.type}</p>
@@ -55,42 +57,47 @@ const ProductItem: React.FC<{ product: Product }> = ({ product }) => (
             ))}
         </div>
         <p className="product__order">Order: {product.order}</p>
-        <AiFillDelete/>
+        <AiFillDelete />
     </div>
 );
 
 const Products: React.FC = () => {
     const { products } = useSelector((state: RootState) => state.products);
-    const [selectedType, setSelectedType] = React.useState("");
+    const [selectedType, setSelectedType] = React.useState('');
 
     const handleTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedType(event.target.value);
     };
 
     const filteredProducts = selectedType
-        ? products.filter(product => product.type === selectedType)
+        ? products.filter((product) => product.type === selectedType)
         : products;
     return (
         <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <div className="products">
-            <h2>Продукты / {products.length}</h2>
-            <select value={selectedType} onChange={handleTypeChange}>
-                <option value="">Все типы</option>
-                <option value="Monitors">Monitors</option>
-                <option value="Phone">Phone</option>
-            </select>
-            <div className="products__wrapper">
-                <div className="products__list">
-                    {filteredProducts.map((product) => (
-                        <ProductItem key={product.id} product={product} />
-                    ))}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+        >
+            <div className="products">
+                <div className="products__title">
+                    <h2>Продукты / {products.length}</h2>
+                    <span>по типу:</span>
+                    <select value={selectedType} onChange={handleTypeChange}>
+                        <option value="">Все типы</option>
+                        <option value="Monitors">Monitors</option>
+                        <option value="Phone">Phone</option>
+                    </select>
+                </div>
+
+                <div className="products__wrapper">
+                    <div className="products__list">
+                        {filteredProducts.map((product) => (
+                            <ProductItem key={product.id} product={product} />
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div></motion.div>
+        </motion.div>
     );
 };
 
